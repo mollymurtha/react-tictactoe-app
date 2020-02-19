5ed9b7e377117349204caf75fcfcf73d765defb2
Molly Murtha <mollymurtha@Mollys-MacBook-Pro-2.local>
2020-02-19 12:22:25 -0500

    #1

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import Game from './Components/TicTacToe/index.js';
+import Login from './Components/login/index.js';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {loggedIn:false,
+      users:[{username:"test",password:"test"}]
+    }
+  }
+  logInUser(loggedInStatus) {
+    this.setState({loggedIn:loggedInStatus});
+  }
+  render() {
+    return (
+      (this.state.register) ?
+        <Register />
+      :
+        (!this.state.loggedIn) ?
+      <Login users={this.state.users}
+        logInUser={() => this.logInUser(true)} />
+      :
+        <Game logOutUser={() => this.logInUser(false)} />
+    )
+  }
+}
+
+export default App;
---
+++ src/Components/login/index.js
@@ -0,0 +1,23 @@
+import React from "react"
+import "./App.css"
+
+class Login extends React.Component {
+  clickHandler() {
+    console.log("User is logging on");
+    console.log(this);
+    for(const user of this.props.users) {
+      if(this.refs.username.value === user.username && 
+        this.refs.password.value === user.password)
+        this.props.logInUser();
+    }
+  }
+  render() {
+  return <div>
+      <input ref="username" type="text"/>
+      <input ref="password" type="password"/>
+      <input type="button" value="Login" onClick={() => this.clickHandler()} />
+    </div>;
+  }
+}
+
+export default Login
